f4d5c70529be23b693f7da761dad8fb2a1de355d
xdvarpunen <ismail.belmostefa@gmail.com>
2018-11-26 21:45:33 +0200

    softkey element

--- docs/page/main.js
+++ docs/page/main.js
@@ -76,85 +76,98 @@ const Header = {
 };
 const SoftwareKey = {
   template: `<div v-bind:style="softkeyStyle">
-                <div ref="softkey-left" v-bind:style="softkeyLeftStyle">{{ leftKeyText }}</div>
-                <div ref="softkey-center" v-bind:style="softkeyCenterStyle">{{ centerKeyText }}</div>
-                <div ref="softkey-right" v-bind:style="softkeyRightStyle">{{ rightKeyText }}</div>
-            </div>`,
+          <div ref="softkey-left" v-bind:style="softkeyLeftStyle">{{ leftKeyText }}</div>
+          <div ref="softkey-center" v-bind:style="softkeyCenterStyle">{{ centerKeyText }}</div>
+          <div ref="softkey-right" v-bind:style="softkeyRightStyle">{{ rightKeyText }}</div>
+      </div>`,
   props: ['leftKeyText', 'centerKeyText', 'rightKeyText'],
   data: function () {
-    return {
-      softkeyStyle: {
-        height: '3rem',
-        width: '100%',
-        backgroundColor: '#dadada',
-        borderTop: '0.1rem solid #ffffff'
-      },
-      softkeyLeftStyle: {
-        display: 'inline',
-        paddingLeft: '0.5rem',
-        width: '7.2rem',
-        textAlign: 'center',
-        fontFamily: 'Open Sans',
-        fontSize: '1.6rem',
-        color: '#323232'
-      },
-      softkeyCenterStyle: {
-        display: 'inline',
-        paddingLeft: '0.5rem',
-        paddingRight: '0.5rem',
-        width: '7.6rem',
-        textAlign: 'center',
-        fontFamily: 'Open Sans',
-        fontWeight: 'bold',
-        fontSize: '1.6rem',
-        color: '#323232'
-      },
-      softkeyRightStyle: {
-        display: 'inline',
-        paddingRight: '0.5rem',
-        width: '7.2rem',
-        textAlign: 'center',
-        fontFamily: 'Open Sans',
-        fontSize: '1.6rem',
-        color: '#323232'
+      return {
+          softkeyStyle: {
+              height: '3rem',
+              width: '100%', // 32rem
+              backgroundColor: '#dadada',
+              borderTop: '0.1rem solid #ffffff',
+              bottom: 0,
+              position: 'fixed',
+              display: 'flex',
+              justifyContent: 'space-between'
+          },
+          softkeyLeftStyle: {
+              display: 'inline',
+              paddingLeft: '0.5rem',
+              width: '7.2rem',
+              textAlign: 'center',
+              fontFamily: 'Open Sans',
+              fontSize: '1.6rem',
+              color: '#323232'
+          },
+          softkeyCenterStyle: {
+              display: 'inline',
+              paddingLeft: '0.5rem',
+              paddingRight: '0.5rem',
+              width: '7.6rem',
+              textAlign: 'center',
+              fontFamily: 'Open Sans',
+              fontWeight: 'bold',
+              fontSize: '1.6rem',
+              color: '#323232'
+          },
+          softkeyRightStyle: {
+              display: 'inline',
+              paddingRight: '0.5rem',
+              width: '7.2rem',
+              textAlign: 'center',
+              fontFamily: 'Open Sans',
+              fontSize: '1.6rem',
+              color: '#323232'
+          }
       }
-    }
   },
   methods: {
-    left: function () { alert('You click on SoftLeft') },
-    center: function () { alert('You click on Enter') },
-    right: function () { alert('You click on SoftRight') },
-    handleKeyDown(evt) {
-      switch (evt.key) {
-        case 'SoftLeft':
-          // Action case press left key
-          this.left();
-          break;
+      left: function () { alert('You click on SoftLeft') },
+      center: function () { alert('You click on Enter') },
+      right: function () { alert('You click on SoftRight') },
+      handleKeyDown(evt) {
+          switch (evt.key) {
+              case 'SoftLeft':
+                  // Action case press left key
+                  this.left();
+                  break;
 
-        case 'SoftRight':
-          // Action case press right key
-          this.right();
-          break;
+              case 'SoftRight':
+                  // Action case press right key
+                  this.right();
+                  break;
 
-        case 'Enter':
-          // Action case press center key
-          this.center();
-          break;
+              case 'Enter':
+                  // Action case press center key
+                  this.center();
+                  break;
+          }
+      },
+      updateSoftKey(props) {
+          const keys = Object.keys(props);
+
+          keys.forEach(function (key) {
+              const button = document.getElementById('softkey-' + key);
+              button.textContent = props[key].label;
+              softkeyCallback[key] = props[key].callback;
+          });
       }
-    },
-
   },
   mounted() {
-    document.addEventListener('keydown', this.handleKeyDown);
+      document.addEventListener('keydown', this.handleKeyDown);
   },
   beforeDestroyed() {
-    document.removeEventListener('keydown', this.handleKeyDown);
+      document.removeEventListener('keydown', this.handleKeyDown);
   }
 };
 const Page = {
   template: `<div v-bind:style="pageStyle">
               <Header header-text="Hello World"></Header>
               <slot></slot>
+              <SoftwareKey left-key-text="back" center-key-text="SELECT" right-key-text="options"></SoftwareKey>
             </div>`,
   data: function () {
     return {
